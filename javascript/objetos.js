88d420543fd11457f3eb8b0cb9d2f36b9b4e0d5d
Binchu <leoalegutierrez97@gmail.com>
2021-10-06 23:37:01 -0300

    se ven las materias en inscripciones, cambios a jquery en el archivo profile.js y cambios minimos en el css del perfil

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -40,7 +40,7 @@ class Materia{
 // materia8.materiaEstado();
 
 const materias24012 = [
-    {nombre: 'matemática', codigo: '28', calif: 5},
+    {nombre: 'Matemática', codigo: '28', calif: 5},
     {nombre: 'Física', codigo: '03', calif: 6},
     {nombre: 'Sociedad y estado', codigo: '24', calif: 8},
     {nombre: 'Introducción al pensamiento científico', codigo: '40', calif: 6},
@@ -49,3 +49,21 @@ const materias24012 = [
     {nombre: 'Anatomía', codigo: '6103', calif: 7},
     {nombre: 'Histología, Biología celular, Embriología y Genética', codigo: '6201', calif: 3}
 ];
+
+class Inscripciones{
+    constructor(nombre, codigo, turno){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.turno = turno.toUpperCase();
+    }
+}
+
+const inscripcion1 = new Inscripciones('Anatomía', '6103', 'mañana');
+const inscripcion2 = new Inscripciones('Histología, Biología celular, Embriología y Genética', '6201', 'tarde');
+const inscripcion3 = new Inscripciones('Bioética I', '6301', 'noche');
+const inscripcion4 = new Inscripciones('Salud Mental', '7540', 'tarde');
+const inscripcion5 = new Inscripciones('Medicina Familiar', '6108', 'mañana');
+
+const inscripciones24012 = [];
+
+inscripciones24012.push(inscripcion1, inscripcion2);
--- javascript/profile.js
+++ javascript/profile.js
@@ -1,5 +1,46 @@
 const logedUser = JSON.parse(localStorage.getItem('usuarioLogeado'));
 
+// qué mostrar en "Perfil"
+$("#informacion").append(`<h3 class="infoTitle">Información</h3>
+<p>Nombre y apellido: ${logedUser.nombre}</p>
+<p>N° de libreta universitaria: ${logedUser.libreta}</p>
+<p>Email: ${logedUser.mail}</p>`);
+
+// qué mostrar en "Materias"
+if(logedUser.libreta != 24012){
+    // materiasContent.innerHTML = `<h3>Tu información todavía no ha sido cargada en el sistema</h3>`;
+    $('#materias').append(`<h3>Tu información todavía no ha sido cargada en el sistema</h3>`);
+} else {
+    for(const materia of materias24012){
+        $('#materias').append(`
+        <div class="materia-details">
+            <p>Materia: ${materia.nombre}</p>
+            <p>Código de la materia: ${materia.codigo}</p>
+            <p>Nota final: ${materia.calif}</p>
+            <p>Estado de la materia: ${materia.estado}</p>
+        </div>
+        `)
+    }
+}
+
+// qué mostrar en "Inscripciones"
+if(logedUser.libreta){
+    if(logedUser.libreta === 24012){
+        for(const inscrip of inscripciones24012){
+            $('#inscripciones').append(`
+            <div class="inscrip-details">
+                <p>Materia: ${inscrip.nombre}<p>
+                <p>Código: ${inscrip.codigo}</p>
+                <p>Turno: ${inscrip.turno}</p>
+            </div>`);
+        }
+    } else {
+        $('#inscripciones').append(`<h3 class="noInscrip">No haz realizado ninguna inscripción aún.</h3>`);
+    }
+} else {
+    $('#inscripciones').append(`<h3 class="noInscrip">Ups! Ha ocurrido un error</h3>`);
+}
+
 function mostrarInfo(){
     infoButton.style.background = '#fff';
     infoButton.style.color = '#265fff';
@@ -12,12 +53,6 @@ function mostrarInfo(){
     inscripButton.style.background = '#265fff';
     inscripButton.style.color = '#fff';
     inscripContent.style.display = 'none';
-
-    infoContent.innerHTML = `<h3 class="infoTitle">Información</h3>
-                                <p>Nombre y apellido: ${logedUser.nombre}</p>
-                                <p>N° de libreta universitaria: ${logedUser.libreta}</p>
-                                <p>Email: ${logedUser.mail}</p>
-                            `;
 }
 
 function mostrarMaterias(){
@@ -32,19 +67,6 @@ function mostrarMaterias(){
     inscripButton.style.background = '#265fff';
     inscripButton.style.color = '#fff';
     inscripContent.style.display = 'none';
-
-    if(logedUser.libreta != 24012){
-        materiasContent.innerHTML = `<h3>Tu información todavía no ha sido cargada en el sistema</h3>`;
-    } else {
-        for(const materia of materias24012){
-            let materiaInfo = document.createElement('div')
-            materiaInfo.innerHTML = `<p>Materia: ${materia.nombre}</p>
-                             <p>Código de la materia: ${materia.codigo}</p>
-                             <p>Nota final: ${materia.calif}</p>
-                             <p>Estado de la materia: ${materia.estado}</p>`;
-            materiasContent.appendChild(materiaInfo);
-        }
-    }
 }
 
 function mostrarInscrip(){
@@ -59,8 +81,6 @@ function mostrarInscrip(){
     materiasButton.style.background = '#265fff';
     materiasButton.style.color = '#fff';
     materiasContent.style.display = 'none';
-
-    console.log('trabajando acá');
 }
 
 function logout(){
@@ -68,18 +88,33 @@ function logout(){
     location.href = '../pages/login.html';
 }
 
-logoutButton.onclick = () => {
+$('#logout-button').on('click', () => {
     logout();
-}
+});
 
-infoButton.onclick = () => {
+$('#info-btn').on('click', () => {
     mostrarInfo();
-}
+});
 
-materiasButton.onclick = () => {
+$('#materias-btn').on('click', () => {
     mostrarMaterias();
-}
+});
 
-inscripButton.onclick = () => {
+$('#inscrip-btn').on('click', () => {
     mostrarInscrip();
-}
+});
+
+/*for(const materia of materias24012){
+        let materiaInfo = document.createElement('div')
+        materiaInfo.innerHTML = `<p>Materia: ${materia.nombre}</p>
+                         <p>Código de la materia: ${materia.codigo}</p>
+                         <p>Nota final: ${materia.calif}</p>
+                         <p>Estado de la materia: ${materia.estado}</p>`;
+        materiasContent.appendChild(materiaInfo);
+    } <- sin jquery*/
+
+/*infoContent.innerHTML = `<h3 class="infoTitle">Información</h3>
+                                <p>Nombre y apellido: ${logedUser.nombre}</p>
+                                <p>N° de libreta universitaria: ${logedUser.libreta}</p>
+                                <p>Email: ${logedUser.mail}</p>
+                            `; <- sin jquery*/     
